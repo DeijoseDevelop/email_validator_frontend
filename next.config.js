392d2065df4c8f6d6ed0e756271998291a4ca8b7
Deiver Vasquez <estudiandovazmore@gmail.com>
2023-12-09 14:16:25 -0500

    .

--- next.config.js
+++ next.config.js
@@ -5,6 +5,7 @@ const nextConfig = {
         styledComponents: true,
     },
     env: {
+        BACKEND_URL: process.env.BACKEND_URL,
         BACKEND_API_KEY: process.env.BACKEND_API_KEY
     }
 }
